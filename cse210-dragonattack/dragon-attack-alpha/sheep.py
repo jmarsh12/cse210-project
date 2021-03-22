b54cf91cc9f04ca5bbb5d085e65bad7c5fdbac9e
Bryson <bryson.berrett19@gmail.com>
2021-03-22 15:18:36 -0600

    Sheep class added and working

---
+++ cse210-dragonattack/dragon-attack-alpha/sheep.py
@@ -0,0 +1,25 @@
+import arcade
+import time
+import constants
+
+class Sheep(arcade.Sprite):
+
+    def __init__(self):
+        super().__init__(constants.SHEEP_IMAGE)
+        self.center_x = (constants.SHEEP_RADIUS * 2) + 10
+        self.center_y = (2 * constants.TERRAIN_HEIGHT) + constants.SHEEP_HEIGHT
+        self.alive = True
+        self.sheep_list = []
+        self.boundary_right = 3
+        
+    
+
+    def move_sheep(self):
+        self.change_x = constants.SHEEP_SPEED 
+        self.center_x += self.change_x
+        if self.center_x >= 5000:
+            self.alive = False
+        if self.center_x == self.boundary_right:
+            self.change_x * -1
+        
+
