607595bb9b34e4506f4da807f5d50ca7aa1a5598
Craig Wright <craigers.wright@gmail.com>
2021-04-03 23:11:57 -0600

    got level_builder class working. There is no missiles shooting at the moment so that needs to be looked at

--- cse210-dragonattack/dragon-attack-alpha/village.py
+++ cse210-dragonattack/dragon-attack-alpha/village.py
@@ -4,11 +4,10 @@ from missile import Missile
 
 
 class Village(arcade.Sprite):
-    def __init__(self,  missile_list):
+    def __init__(self):
         super().__init__(constants.VILLAGE_IMAGE)
-
         self.bottom = 80
-        self.missile_list = missile_list
+        self.missile_list = []
         
     def add_missile(self, delta_time:float):
         missile = Missile(self.center_x, self.center_y)
